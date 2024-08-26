2a4d7b28898bd365eb6daa489992f74ff7e4320c
avk19 <amol.khandare@gmail.com>
2024-08-26 17:08:22 +0530

    Update LdapService.java

--- LdapService.java
+++ LdapService.java
@@ -1,53 +1,52 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapNameBuilder;
 import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.stereotype.Service;
-import org.springframework.ldap.core.AttributesMapper;
 
-import javax.naming.directory.SearchControls;
+import javax.naming.Name;
 import javax.naming.directory.Attributes;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LdapService {
 
+    private final LdapTemplate ldapTemplate;
+
     @Autowired
-    private LdapTemplate ldapTemplate;
+    public LdapService(LdapTemplate ldapTemplate) {
+        this.ldapTemplate = ldapTemplate;
+    }
 
     public List<String> getGroupMembers(String groupName) {
         EqualsFilter filter = new EqualsFilter("cn", groupName);
-        
-        return ldapTemplate.search("", filter.encode(), getGroupMemberAttributesMapper());
-    }
 
-    private AttributesMapper<String> getGroupMemberAttributesMapper() {
-        return attrs -> {
+        List<String> memberDns = ldapTemplate.search("", filter.encode(), (Attributes attrs) -> {
             if (attrs != null) {
-                try {
-                    return attrs.get("member").get().toString();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                return attrs.get("member").get().toString();
             }
             return null;
-        };
+        });
+
+        List<String> samAccountNames = new ArrayList<>();
+        for (String memberDn : memberDns) {
+            String samAccountName = getSamAccountName(memberDn);
+            if (samAccountName != null) {
+                samAccountNames.add(samAccountName);
+            }
+        }
+        return samAccountNames;
     }
 
-    public String getSamAccountName(String memberDn) {
-        EqualsFilter filter = new EqualsFilter("objectClass", "user");
+    private String getSamAccountName(String memberDn) {
+        Name dn = LdapNameBuilder.newInstance(memberDn).build();
 
-        List<String> result = ldapTemplate.search(memberDn, filter.encode(), SearchControls.OBJECT_SCOPE, 
-            new String[]{"sAMAccountName"}, (Attributes attrs) -> {
-                if (attrs != null) {
-                    try {
-                        return attrs.get("sAMAccountName").get().toString();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                return null;
-            });
-
-        return result.isEmpty() ? null : result.get(0);
+        return ldapTemplate.lookup(dn, new String[]{"sAMAccountName"}, (Attributes attrs) -> {
+            if (attrs != null) {
+                return attrs.get("sAMAccountName").get().toString();
+            }
+            return null;
+        });
     }
 }
