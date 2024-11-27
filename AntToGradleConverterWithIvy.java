c952d626e21f9e830cf6d481cfd4b78fb2b98ff7
avk19 <amol.khandare@gmail.com>
2024-11-27 12:34:55 +0530

    Update AntToGradleConverterWithIvy.java

--- AntToGradleConverterWithIvy.java
+++ AntToGradleConverterWithIvy.java
@@ -1,8 +1,9 @@
 import org.w3c.dom.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.*;
-import java.nio.file.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -50,18 +51,6 @@ public class AntToGradleConverterWithIvy {
             gradleContent.append("rootProject.name = '").append(projectName).append("'\n\n");
         }
 
-        // Parse included files
-        NodeList includeNodes = document.getElementsByTagName("import");
-        gradleContent.append("// Imported Builds\n");
-        for (int i = 0; i < includeNodes.getLength(); i++) {
-            Element include = (Element) includeNodes.item(i);
-            String filePath = include.getAttribute("file");
-            if (!filePath.isEmpty()) {
-                String resolvedPath = resolvePath(antBuildFilePath, filePath);
-                gradleContent.append(convertAntToGradle(resolvedPath));
-            }
-        }
-
         // Parse properties
         NodeList propertyNodes = document.getElementsByTagName("property");
         gradleContent.append("// Properties\n");
@@ -75,67 +64,39 @@ public class AntToGradleConverterWithIvy {
         }
         gradleContent.append("\n");
 
-        // Parse Ivy configuration
-        gradleContent.append(parseIvyConfiguration(document));
-
         // Parse targets
         NodeList targetNodes = document.getElementsByTagName("target");
         gradleContent.append("// Tasks\n");
         for (int i = 0; i < targetNodes.getLength(); i++) {
             Element target = (Element) targetNodes.item(i);
             String targetName = target.getAttribute("name");
-            if (!targetName.isEmpty()) {
-                gradleContent.append("task ").append(targetName).append(" {\n");
-                gradleContent.append("    doLast {\n");
-                gradleContent.append("        println 'Executing ").append(targetName).append("'\n");
-                gradleContent.append("    }\n");
-                gradleContent.append("}\n\n");
+            gradleContent.append("task ").append(targetName).append(" {\n");
+            gradleContent.append("    doLast {\n");
+            gradleContent.append("        println 'Executing ").append(targetName).append("'\n");
+            gradleContent.append("    }\n");
+
+            // Process <ant> tasks within this target
+            NodeList childNodes = target.getChildNodes();
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node childNode = childNodes.item(j);
+                if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element childElement = (Element) childNode;
+                    if (childElement.getTagName().equals("ant")) {
+                        String antFile = childElement.getAttribute("antfile");
+                        if (!antFile.isEmpty()) {
+                            gradleContent.append("    // Converted from <ant antfile=\"").append(antFile).append("\">\n");
+                            gradleContent.append("    includeBuild '").append(antFile).append("'\n");
+
+                            // Recursively process the referenced Ant file
+                            String nestedGradleContent = convertAntToGradle(new File(xmlFile.getParent(), antFile).getAbsolutePath());
+                            gradleContent.append(nestedGradleContent);
+                        }
+                    }
+                }
             }
+            gradleContent.append("}\n\n");
         }
 
         return gradleContent.toString();
-    }
-
-    private static String parseIvyConfiguration(Document document) {
-        StringBuilder ivyContent = new StringBuilder();
-
-        // Parse Ivy repositories
-        NodeList ivySettingsNodes = document.getElementsByTagName("ivy:settings");
-        ivyContent.append("// Repositories\n");
-        ivyContent.append("repositories {\n");
-        for (int i = 0; i < ivySettingsNodes.getLength(); i++) {
-            Element ivySettings = (Element) ivySettingsNodes.item(i);
-            String url = ivySettings.getAttribute("url");
-            if (!url.isEmpty()) {
-                ivyContent.append("    ivy {\n");
-                ivyContent.append("        url '").append(url).append("'\n");
-                ivyContent.append("    }\n");
-            }
-        }
-        ivyContent.append("}\n\n");
-
-        // Parse Ivy dependencies
-        NodeList ivyResolveNodes = document.getElementsByTagName("ivy:resolve");
-        ivyContent.append("// Dependencies\n");
-        ivyContent.append("dependencies {\n");
-        for (int i = 0; i < ivyResolveNodes.getLength(); i++) {
-            Element ivyResolve = (Element) ivyResolveNodes.item(i);
-            String org = ivyResolve.getAttribute("org");
-            String module = ivyResolve.getAttribute("module");
-            String rev = ivyResolve.getAttribute("rev");
-            if (!org.isEmpty() && !module.isEmpty() && !rev.isEmpty()) {
-                ivyContent.append("    implementation '").append(org).append(":").append(module).append(":").append(rev).append("'\n");
-            }
-        }
-        ivyContent.append("}\n\n");
-
-        return ivyContent.toString();
-    }
-
-    private static String resolvePath(String baseFilePath, String relativePath) {
-        File baseFile = new File(baseFilePath);
-        File parentDir = baseFile.getParentFile();
-        File resolvedFile = new File(parentDir, relativePath);
-        return resolvedFile.getAbsolutePath();
     }
 }
